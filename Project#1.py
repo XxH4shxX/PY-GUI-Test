945f8c20ab2e9f6faa773c0953732a8dcc951491
XxTrinityxX <enriquevangola@gmail.com>
2017-04-24 10:53:54 -0700

    Update Project#1.py

--- Project#1.py
+++ Project#1.py
@@ -1,1 +1,10 @@
-import Tkinter
+import tkinter
+import tkMessageBox
+top = tkinter.Tk()
+
+top.mainloop()
+
+def mshshow():
+  tkMessageBox.showinfo("First Attempt","GUI test")
+  
+B = Tkinter.Button(top, text = "Hello",)
